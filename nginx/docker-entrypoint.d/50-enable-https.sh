8643820b8d8c28d7cd16596c2fccb32a96206057
andres-saa <andres19f@gmail.com>
2025-08-08 03:59:16 +0100

    ssl

--- nginx/docker-entrypoint.d/50-enable-https.sh
+++ nginx/docker-entrypoint.d/50-enable-https.sh
@@ -4,17 +4,20 @@ set -eu
   TARGET="/etc/nginx/conf.d/reverse-proxy.https.conf"
   DISABLED="/etc/nginx/conf.d/reverse-proxy.https.conf.disabled"
   FIRST_DOMAIN="usa.salchimonster.com"
-  # Espera a que exista el primer cert (emisión inicial)
-  while [ ! -f "/etc/letsencrypt/live/$FIRST_DOMAIN/fullchain.pem" ]; do
-    sleep 5
-  done
-  if [ -f "$DISABLED" ]; then
-    cp "$DISABLED" "$TARGET"
+  # Si no hay dominios, no hacemos nada
+  if [ -n "$FIRST_DOMAIN" ]; then
+    # Espera a que exista el primer cert (emisión inicial)
+    while [ ! -f "/etc/letsencrypt/live/$FIRST_DOMAIN/fullchain.pem" ]; do
+      sleep 5
+    done
+    if [ -f "$DISABLED" ]; then
+      cp "$DISABLED" "$TARGET"
+    fi
+    nginx -t && nginx -s reload || true
+    # Recargas periódicas para tomar renovaciones
+    while :; do
+      sleep 21600  # 6h
+      nginx -s reload || true
+    done
   fi
-  nginx -t && nginx -s reload || true
-  # Recargas periódicas para tomar renovaciones
-  while :; do
-    sleep 21600  # 6h
-    nginx -s reload || true
-  done
 ) &
